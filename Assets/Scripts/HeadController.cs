db91c1f488abaa72654eb83b1f3af6a122885122
Krzysztof Kucharczyk <toficzak@gmail.com>
2016-12-11 18:57:19 +0100

    Extending body at <Space>, body is following the head and removing last part at move.

--- Assets/Scripts/HeadController.cs
+++ Assets/Scripts/HeadController.cs
@@ -8,20 +8,28 @@ public class HeadController : MonoBehaviour {
 	private Rigidbody2D rigidbody;   // Uchwyt do Rigidbody obiektu
 	private float nextMove;   // Czas nastepnego ruchu
 	private Vector3 movement;   // Kierunek ruchu 
+	private bool grow;   // tymczasowe: czy rosnąć?
+	public GameObject snakeBody;
+	private LinkedList<GameObject> body;
+	private Vector2 lastHeadPosition;
+	int bodySize = 0;
 
 	void Start () 
 	{
 		rigidbody = GetComponent<Rigidbody2D>();
 		movement = new Vector3 (0.0f, 0.0f);
+		body = new LinkedList<GameObject> ();
+		lastHeadPosition = new Vector2 (0.0f, 0.0f);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		// Zmiana kierunku ruchu
-		//------------------------
+
+		lastHeadPosition = rigidbody.transform.position;
+
 		if (Input.anyKeyDown) 
 		{
-			if (Input.GetKey (KeyCode.UpArrow) && movement.y != -1)
+			if (Input.GetKey(KeyCode.UpArrow) && movement.y != -1)
 				movement = new Vector2 (0.0f, 1);
 			else if (Input.GetKey (KeyCode.RightArrow) && movement.x != -1)
 				movement = new Vector2 (1, 0.0f);
@@ -29,12 +37,21 @@ public class HeadController : MonoBehaviour {
 				movement = new Vector2 (0.0f, -1);
 			else if (Input.GetKey (KeyCode.LeftArrow) && movement.x != 1)
 				movement = new Vector2 (-1, 0.0f);
+			else if (Input.GetKey (KeyCode.Space))
+				grow = true;
 		}
-		//------------------------
+
 		if (Time.time > nextMove) {
 			nextMove = Time.time + (1 / speed);
 
 			Move ();
+			if(bodySize > 0)
+				UpdateBodyLocation ();
+
+			if (grow)
+				Grow ();
+
+			grow = false;
 		}
 	
 	}
@@ -42,14 +59,18 @@ public class HeadController : MonoBehaviour {
 	void Move ()
 	{
 		Vector2 nextPosition = rigidbody.transform.position + movement;
-
-		//GetComponent<BoxCollider2D>().enabled = false;
-		//RaycastHit2D hit = Physics2D.Linecast (rigidbody.transform.position, nextPosition, LayerMask.GetMask ("Default"));
-		//GetComponent<BoxCollider2D>().enabled = true;
-
-
 		rigidbody.transform.position = nextPosition;
-
 	}
 
+	void Grow ()
+	{
+		body.AddLast((GameObject) Instantiate(snakeBody, rigidbody.transform.position - movement, Quaternion.identity));
+		bodySize++;
+	}
+
+	void UpdateBodyLocation() {
+		body.AddAfter (body.First, (GameObject)Instantiate (snakeBody, rigidbody.transform.position - movement, Quaternion.identity));
+		Destroy (body.Last.Value);
+		body.RemoveLast ();
+	}
 }
