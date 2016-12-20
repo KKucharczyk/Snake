b6fe0b88273c91ae0557720f27b10b88d5b099a7
Marrkix <MarrkixPL@gmail.com>
2016-12-20 18:30:57 +0100

    Snake_v0.1

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,44 +4,24 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour 
 {
-	public GameObject applePrefab;
-	private GameObject apple;
+	public GameObject foodControllerPrefab;
+	private GameObject foodController;
+
+	public GameObject mapControllerPrefab;
+	private GameObject mapController;
 
 	public Vector2 mapSize;
 
 	// Use this for initialization
 	void Start () 
 	{
-		apple = null;
+		foodController = Instantiate (foodControllerPrefab);
+		mapController = Instantiate (mapControllerPrefab);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (apple == null)
-			SpawnApple ();
-	}
-
-	void SpawnApple ()
-	{
-		Vector2 spawnPosition;
-		spawnPosition = RandomPosition ();
-		if (IsFree (spawnPosition))
-			apple = Instantiate (applePrefab, spawnPosition, Quaternion.identity); 
-	}
-
-	Vector2 RandomPosition ()
-	{
-		return new Vector2 
-		(
-			(int)Random.Range (-mapSize.x, mapSize.x),
-			(int)Random.Range (-mapSize.y, mapSize.y)
-		);
-	}
-
-	bool IsFree (Vector2 position)
-	{
-		RaycastHit2D hit = Physics2D.Linecast (position, position, LayerMask.GetMask ("Default"));
-		return (hit.transform == null);
+		
 	}
 }
---
+++ Assets/Scripts/MapController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour {
+
+	public GameObject groundPrefab;
+	public GameObject wallPrefab;
+
+	private GameController gameController;
+
+	private Vector2 mapSize;
+
+	// Use this for initialization
+	void Start () 
+	{
+		GameObject gC = GameObject.FindWithTag ("GameController");
+		if (gC != null) 
+		{
+			gameController = gC.GetComponent<GameController> ();
+			mapSize = gameController.mapSize;
+		} 
+		else 
+		{
+			Debug.Log ("MapController.Start() : Cannot find GameController");
+			Destroy (gameObject);
+		}
+
+		CreateMap ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+
+	public void CreateMap ()
+	{
+		for (float x = -mapSize.x; x <= mapSize.x; ++x) 
+		{
+			for (float y = -mapSize.y; y <= mapSize.y; ++y) 
+			{
+				Instantiate (groundPrefab, new Vector2 (x, y), Quaternion.identity, transform);
+				if ((x == -mapSize.x || x == mapSize.x) || (y == -mapSize.y || y == mapSize.y))
+					Instantiate (wallPrefab, new Vector2 (x, y), Quaternion.identity, transform);	
+			}
+		}
+	}
+}
