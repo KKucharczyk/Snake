13bcf2969afd68bd761de5bf18a221dff2227b51
Marrkix <MarrkixPL@gmail.com>
2017-01-22 03:56:40 +0100

    Snake's turning with bad momentum fixed

--- Assets/Scripts/Snake/LiveRoutineController.cs
+++ Assets/Scripts/Snake/LiveRoutineController.cs
@@ -7,7 +7,9 @@ public class LiveRoutineController : MonoBehaviour
 	private float nextMove = 0.0f;
 	public float speed;
 	private bool grow;
-	private bool notMoved= true;
+
+	private Direction currentDirection = Direction.UNDEFINED;
+	private Direction newDirection = Direction.UNDEFINED;
 
 	private GameObject snakeHandler;
 	public GameObject snakePrefab;
@@ -21,18 +23,15 @@ public class LiveRoutineController : MonoBehaviour
 
 	void Update ()
 	{
-		if (Input.anyKeyDown && notMoved) {
-			analyzeKeyPressed ();
-			notMoved = false;
-		}
-
-		snakeController.calculateNewHeadPosition ();
+		analyzeKeyPressed ();
 
 		if (Time.time > nextMove) 
 		{
-			nextMove = Time.time + (1 / speed);
+			nextMove = Time.time + (0.8f - 0.05f * speed);
+
+			updateSnakeOnNewDirection (newDirection, (int) newDirection);
+			snakeController.calculateNewHeadPosition ();
 			snakeController.moveHead ();
-			notMoved = true;
 
 			if (grow || snakeController.isGrowing ()) {
 				snakeController.grow ();
@@ -44,20 +43,21 @@ public class LiveRoutineController : MonoBehaviour
 	}
 
 	void analyzeKeyPressed() {
-		if (Input.GetKey (KeyCode.UpArrow)) {
-			updateSnakeOnNewDirection (Direction.UP, 0);
-		}  else if (Input.GetKey (KeyCode.DownArrow)) {
-			updateSnakeOnNewDirection (Direction.DOWN, 1);
-		} else if (Input.GetKey (KeyCode.LeftArrow)) {
-			updateSnakeOnNewDirection (Direction.LEFT, 2);
-		} else if (Input.GetKey (KeyCode.RightArrow)) {
-			updateSnakeOnNewDirection (Direction.RIGHT, 3);
+		if (Input.GetKey (KeyCode.UpArrow) && currentDirection != Direction.DOWN) {
+			newDirection = Direction.UP;
+		}  else if (Input.GetKey (KeyCode.DownArrow) && currentDirection != Direction.UP) {
+			newDirection = Direction.DOWN;
+		} else if (Input.GetKey (KeyCode.LeftArrow) && currentDirection != Direction.RIGHT) {
+			newDirection = Direction.LEFT;
+		} else if (Input.GetKey (KeyCode.RightArrow) && currentDirection != Direction.LEFT) {
+			newDirection = Direction.RIGHT;
 		} else if (Input.GetKey (KeyCode.Space)) {
 			grow = true;
 		}
 	}
 
 	void updateSnakeOnNewDirection(Direction direction, int spriteIndex) {
+		currentDirection = newDirection;
 		if (direction != DirectionMethods.getOpposite(snakeController.getCurrentHeadDirection ())) {
 			snakeController.setPreviousHeadDirection (snakeController.getCurrentHeadDirection ());
 			snakeController.setCurrentHeadDirection (direction);
