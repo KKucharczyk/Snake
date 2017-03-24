7abd75a81e684f11b9f3104666b18036b0134d44
Krzysztof Kucharczyk <krzysztof.kucharczyk@hotmail.com>
2017-03-24 22:56:17 +0100

    Enum for body sprites.

---
+++ Assets/Scripts/Snake/BodySprite.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/// <summary>
+/// Enum determines body sprite and isolates it from main logic.
+/// 
+/// Name convention: first direction is the direction before turn. Second one is direction right after the turn.
+/// 
+/// TO DO: maybe connect it somehow with spriteRenderer? I cannot look at such a hardcoded values.
+/// </summary>
+public enum BodySprite
+{
+    UNDEFINED = -1,
+    HORIZONTAL = 0,
+    VERTICAL = 1,
+    NORTH_WEST = 2,
+    NORTH_EAST = 4,
+    SOUTH_WEST = 3,
+    SOUTH_EAST = 5
+}
+
+public static class BodySpritesUtils
+{
+    public static int toInt(this BodySprite bodySprite)
+    {
+        return Convert.ToInt32(bodySprite);
+    }
+}
+
+
